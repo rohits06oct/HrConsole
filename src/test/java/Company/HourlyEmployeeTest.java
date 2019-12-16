a0f35e2c80470a010b10637e6f6331c5d8a4d292
rohits06oct <rohits.06oct@gmail.com>
2019-12-16 10:46:00 +0530

    Changes

--- src/test/java/Company/HourlyEmployeeTest.java
+++ src/test/java/Company/HourlyEmployeeTest.java
@@ -21,14 +21,30 @@ public class HourlyEmployeeTest {
         Assert.assertEquals(hourlyEmployee.getMonthlySalary(), pay);
     }
 
+    @Test(dataProvider = "hourlyEmployeeClassWithRateWrongLastName", dataProviderClass = DataProviderClass.class, priority = 4, expectedExceptions = {
+            IllegalArgumentException.class }, expectedExceptionsMessageRegExp = "Last name is invalid")
+    public void testHourlyEmployeeWithRate_WrongLastName(int empId, String firstName, String lastName, Date hireDate, int rate,
+                                                         EmployeeType type, int pay) {
+        hourlyEmployee = new HourlyEmployee(empId, firstName, lastName, hireDate, rate, type);
+        Assert.assertEquals(hourlyEmployee.getMonthlySalary(), pay);
+    }
+
     /**
      * take all the values from hourly employee without rate and pass to the constructor and Assert
      * both the pay's
      */
-    @Test(dataProvider = "hourlyEmployeeClassWithoutRate", dataProviderClass = DataProviderClass.class, priority = 2, expectedExceptions = {
+    @Test(dataProvider = "hourlyEmployeeClassWithoutRateWithWrongEmpId", dataProviderClass = DataProviderClass.class, priority = 2, expectedExceptions = {
             IllegalArgumentException.class }, expectedExceptionsMessageRegExp = "EmpId is invalid")
-    public void testHourlyEmployeeWithOutRatePay_Correct(int empId, String firstName, String lastName, Date hireDate,
+    public void testHourlyEmployeeWithOutRate_WrongEmpId(int empId, String firstName, String lastName, Date hireDate,
                                                          EmployeeType type, int pay) {
+        hourlyEmployee = new HourlyEmployee(empId, firstName, lastName, hireDate, type);
+        Assert.assertEquals(hourlyEmployee.getMonthlySalary(), pay);
+    }
+
+    @Test(dataProvider = "hourlyEmployeeClassWithoutRateWithWrongFirstName", dataProviderClass = DataProviderClass.class, priority = 3, expectedExceptions = {
+            IllegalArgumentException.class }, expectedExceptionsMessageRegExp = "First name is invalid")
+    public void testHourlyEmployeeWithOutRate_WrongFirstName(int empId, String firstName, String lastName, Date hireDate,
+                                                             EmployeeType type, int pay) {
         hourlyEmployee = new HourlyEmployee(empId, firstName, lastName, hireDate, type);
         Assert.assertEquals(hourlyEmployee.getMonthlySalary(), pay);
     }
--- src/test/java/Company/SalariedEmployeeTest.java
+++ src/test/java/Company/SalariedEmployeeTest.java
@@ -15,8 +15,16 @@ public class SalariedEmployeeTest {
      */
     @Test(dataProvider = "salariedEmployeeClassWithExperience", dataProviderClass = DataProviderClass.class, priority = 2, expectedExceptions = {
             IllegalArgumentException.class }, expectedExceptionsMessageRegExp = "Experience is Invalid")
-    public void testSalariedEmployeeWithRatePay_Correct(int empId, String firstName, String lastName, Date hireDate,
-                                                        int experience, EmployeeType type, int pay) {
+    public void testSalariedEmployeeWithExperience(int empId, String firstName, String lastName, Date hireDate, int experience,
+                                                   EmployeeType type, int pay) {
+        salariedEmployee = new SalariedEmployee(empId, firstName, lastName, hireDate, experience, type);
+        Assert.assertEquals(salariedEmployee.getMonthlySalary(), pay);
+    }
+
+    @Test(dataProvider = "salariedEmployeeClassWithExperienceWrongLastName", dataProviderClass = DataProviderClass.class, priority = 2, expectedExceptions = {
+            IllegalArgumentException.class }, expectedExceptionsMessageRegExp = "Last name is invalid")
+    public void testSalariedEmployeeWithExperience_WrongLastName(int empId, String firstName, String lastName, Date hireDate,
+                                                                 int experience, EmployeeType type, int pay) {
         salariedEmployee = new SalariedEmployee(empId, firstName, lastName, hireDate, experience, type);
         Assert.assertEquals(salariedEmployee.getMonthlySalary(), pay);
     }
@@ -25,10 +33,18 @@ public class SalariedEmployeeTest {
      * take all the values from salaried employee without experience and pass to the constructor and
      * Assert both the pay's
      */
-    @Test(dataProvider = "salariedEmployeeClassWithoutExperience", dataProviderClass = DataProviderClass.class, priority = 4, expectedExceptions = {
+    @Test(dataProvider = "salariedEmployeeClassWithoutExperienceWrongEmpId", dataProviderClass = DataProviderClass.class, priority = 4, expectedExceptions = {
             IllegalArgumentException.class }, expectedExceptionsMessageRegExp = "EmpId is invalid")
-    public void testSalariedEmployeeWithOutRatePay_Correct(int empId, String firstName, String lastName, Date hireDate,
-                                                           EmployeeType type, int pay) {
+    public void testSalariedEmployeeWithOutExperience_WrongEmpId(int empId, String firstName, String lastName, Date hireDate,
+                                                                 EmployeeType type, int pay) {
+        salariedEmployee = new SalariedEmployee(empId, firstName, lastName, hireDate, type);
+        Assert.assertEquals(salariedEmployee.getMonthlySalary(), pay);
+    }
+
+    @Test(dataProvider = "salariedEmployeeClassWithoutExperienceWrongFirstName", dataProviderClass = DataProviderClass.class, priority = 3, expectedExceptions = {
+            IllegalArgumentException.class }, expectedExceptionsMessageRegExp = "First name is invalid")
+    public void testSalariedEmployeeWithOutExperience_WrongFirstName(int empId, String firstName, String lastName, Date hireDate,
+                                                                     EmployeeType type, int pay) {
         salariedEmployee = new SalariedEmployee(empId, firstName, lastName, hireDate, type);
         Assert.assertEquals(salariedEmployee.getMonthlySalary(), pay);
     }
