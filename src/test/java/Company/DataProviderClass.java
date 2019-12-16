feebbc48182a05d27678e8f7877194295b224efa
rohits06oct <rohits.06oct@gmail.com>
2019-12-16 10:42:48 +0530

    Changes

--- src/test/java/Company/DataProviderClass.java
+++ src/test/java/Company/DataProviderClass.java
@@ -11,10 +11,14 @@ public class DataProviderClass {
     /**
      * Data providers for Hourly Employee Give all values to without rate constructor
      */
-    @DataProvider(name = "hourlyEmployeeClassWithoutRate")
-    public static Object[][] dataHourlyEmployeeClassWithoutRate() {
-        return new Object[][] { { 0, "", "Kumar", hireDate, EmployeeType.Hourly, 11000 },
-                { 423, "Kanta", "Raj", hireDate, EmployeeType.Hourly, 11000 } };
+    @DataProvider(name = "hourlyEmployeeClassWithoutRateWithWrongEmpId")
+    public static Object[][] dataHourlyEmployeeClassWithoutRateWithWrongEmpId() {
+        return new Object[][] { { 0, "Brajesh", "Kumar", hireDate, EmployeeType.Hourly, 11000 } };
+    }
+
+    @DataProvider(name = "hourlyEmployeeClassWithoutRateWithWrongFirstName")
+    public static Object[][] dataHourlyEmployeeClassWithoutRateWithWrongFirstName() {
+        return new Object[][] { { 423, "", "Raj", hireDate, EmployeeType.Hourly, 11000 } };
     }
 
     /**
@@ -22,26 +26,38 @@ public class DataProviderClass {
      */
     @DataProvider(name = "hourlyEmployeeClassWithRate")
     public static Object[][] dataHourlyEmployeeClassWithRate() {
-        return new Object[][] { { 232, "Brajesh", "Kumar", hireDate, 0, EmployeeType.Hourly, 25300 },
-                { 893, "Kanta", "Raj", hireDate, 600, EmployeeType.Hourly, 66000 } };
+        return new Object[][] { { 232, "Brajesh", "Kumar", hireDate, 0, EmployeeType.Hourly, 25300 } };
+    }
+
+    @DataProvider(name = "hourlyEmployeeClassWithRateWrongLastName")
+    public static Object[][] dataHourlyEmployeeClassWithRate_WrongLastName() {
+        return new Object[][] { { 893, "Kanta", "", hireDate, 600, EmployeeType.Hourly, 66000 } };
     }
 
     /**
      * Data providers for Salaried Employee Give all values to without experience constructor
      */
-    @DataProvider(name = "salariedEmployeeClassWithoutExperience")
-    public static Object[][] dataSalariedEmployeeClassWithoutRate() {
-        return new Object[][] { { 0, "Suraj", "Tomar", hireDate, EmployeeType.Salaried, 25000 },
-                { 423, "Rahul", "Ojha", hireDate, EmployeeType.Salaried, 25000 } };
+    @DataProvider(name = "salariedEmployeeClassWithoutExperienceWrongEmpId")
+    public static Object[][] dataSalariedEmployeeClassWithoutExperience_WrongEmpId() {
+        return new Object[][] { { 0, "Suraj", "Tomar", hireDate, EmployeeType.Salaried, 25000 } };
+    }
+
+    @DataProvider(name = "salariedEmployeeClassWithoutExperienceWrongFirstName")
+    public static Object[][] dataSalariedEmployeeClassWithoutExperience_WrongFirstName() {
+        return new Object[][] { { 423, "", "Ojha", hireDate, EmployeeType.Salaried, 25000 } };
     }
 
     /**
      * Data providers for Salaried Employee Give all values to with experience constructor
      */
     @DataProvider(name = "salariedEmployeeClassWithExperience")
-    public static Object[][] dataSalariedEmployeeClassWithRate() {
-        return new Object[][] { { 36, "Suraj", "Tomar", hireDate, 50, EmployeeType.Salaried, 37500 },
-                { 100, "Rahul", "Ojha", hireDate, 4, EmployeeType.Salaried, 35000 } };
+    public static Object[][] dataSalariedEmployeeClassWithExperience() {
+        return new Object[][] { { 36, "Suraj", "Tomar", hireDate, 50, EmployeeType.Salaried, 37500 } };
+    }
+
+    @DataProvider(name = "salariedEmployeeClassWithExperienceWrongLastName")
+    public static Object[][] dataSalariedEmployeeClassWithExperience_WrongLastName() {
+        return new Object[][] { { 100, "Rahul", "", hireDate, 4, EmployeeType.Salaried, 35000 } };
     }
 
     /**
